e7607d21f960eed9ac75d39ec1c26d5d266d85ea
Maksim Volodin <gedjer.dev@gmail.com>
2025-07-25 17:16:25 +0300

    TEST-1: initial commit

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("This is the main package for lesson 2.")
+	fmt.Println("It will contain the main application logic.")
+	fmt.Println("You can run this application to see how it works with the updated user handling.")
+	fmt.Println("Make sure to implement the necessary handlers and use cases as per the new structure.")
+	fmt.Println("Refer to the previous lessons for guidance on how to structure your code.")
+}
